8ab96356beaa49f36df65c2722c8c8c4a5e138e2
Eric Chu <eytchu@gmail.com>
2014-11-18 00:05:10 -0800

    changed location of wheels

--- appveyor/move-wheel.py
+++ appveyor/move-wheel.py
@@ -3,7 +3,7 @@ import shutil
 import glob
 from conda_build import config
 
-wheel_path = os.path.join(config.croot, "work\python\dist\*.whl")
+wheel_path = os.path.join(config.croot, "work\dist\*.whl")
 wheel = glob.glob(wheel_path)[0]
 
 shutil.move(wheel, '.')
