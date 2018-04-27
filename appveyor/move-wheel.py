4908a1172a34cbae61e8af061154fc3eeeaea953
Eric Chu <eytchu@gmail.com>
2018-04-26 23:31:42 -0700

    [appveyor/move-wheel.py] Grab wheels from new dir
    The work directory is moved / renamed, so we attempt to glob for it.
    Unclear what we might pick up, but hopefully the environment is
    isolated!

--- appveyor/move-wheel.py
+++ appveyor/move-wheel.py
@@ -3,7 +3,8 @@ import shutil
 import glob
 from conda_build import config
 
-wheel_path = os.path.join(config.croot, "work\dist\*.whl")
+wheel_path = os.path.join(config.croot, "ecos_*", "work_moved_ecos*", "dist", "*.whl")
+print("Looking for wheel in path matching '{}'".format(wheel_path))
 wheel = glob.glob(wheel_path)[0]
 
 shutil.move(wheel, '.')
