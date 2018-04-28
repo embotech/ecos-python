441efb96886d0e323c2a0d3a8ef61547549c5753
Eric Chu <eytchu@gmail.com>
2018-04-27 23:18:43 -0700

    [setup.py] Change mechanism for setting __NUMPY_SETUP__
    Following https://docs.python.org/2/reference/executionmodel.html,
    `__builtins__` can mean different things depending on how setup.py is
    executed. This code attempts a workaround following
    
    https://github.com/singingwolfboy/nose-progressive/commit/d8dce093910724beff86081327565b11ea28dfbc

--- setup.py
+++ setup.py
@@ -41,6 +41,12 @@ _ecos = Extension('_ecos', libraries = lib,
                     ] + glob('ecos/external/amd/src/*.c')
                       + glob('ecos/ecos_bb/*.c'))       # glob bb source files
 
+def set_builtin(name, value):
+    if isinstance(__builtins__, dict):
+        __builtins__[name] = value
+    else:
+        setattr(__builtins__, name, value)
+
 class build_ext(_build_ext):
     """ This custom class for building extensions exists so we can force
     a numpy install before building the extension, thereby giving us
@@ -49,7 +55,7 @@ class build_ext(_build_ext):
     def finalize_options(self):
         _build_ext.finalize_options(self)
         # Prevent numpy from thinking it is still in its setup process:
-        __builtins__.__NUMPY_SETUP__ = False
+        set_builtin("__NUMPY_SETUP__", False)
         import numpy
         self.include_dirs.append(numpy.get_include())
 
