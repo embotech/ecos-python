9ece3f8ccef771ad6844b1211b005d60de481c32
Steven Diamond <stevend2@stanford.edu>
2020-10-20 19:15:52 -0700

    user 2.0

--- travis/build-osx-wheels.sh
+++ travis/build-osx-wheels.sh
@@ -2,8 +2,8 @@
 set -e -x
 
 # Compile wheels
-$PYTHON -m --user pip install nose coverage
-$PYTHON -m --user pip wheel . -w wheelhouse/
+$PYTHON -m pip install --user nose coverage
+$PYTHON -m pip wheel . -w wheelhouse/
 
-$PYTHON -m --user pip install ecos --no-index -f wheelhouse
-$PYTHON -m --user nose --with-cover --cover-package=ecos src/test_interface.py src/test_interface_bb.py
+$PYTHON -m pip install --user ecos --no-index -f wheelhouse
+$PYTHON -m nose --with-cover --cover-package=ecos src/test_interface.py src/test_interface_bb.py
