1496dc60e2e02e006172ed2806febad73568361e
Eric Chu <eytchu@gmail.com>
2021-01-16 12:14:15 -0800

    OSX build with user

--- travis/build-osx-wheels.sh
+++ travis/build-osx-wheels.sh
@@ -2,8 +2,8 @@
 set -e -x
 
 # Compile wheels
-$PYTHON -m pip install nose coverage
+$PYTHON -m pip install nose coverage --user
 $PYTHON -m pip wheel . -w wheelhouse/
 
-$PYTHON -m pip install ecos --no-index -f wheelhouse
+$PYTHON -m pip install ecos --no-index -f wheelhouse --user
 $PYTHON -m nose --with-cover --cover-package=ecos src/test_interface.py src/test_interface_bb.py
